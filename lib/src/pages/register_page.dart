3fbd28d54797cc564aed6d537956e85d551ae6c8
julian <juliandanielfranco@gmail.com>
2020-11-27 12:40:45 -0500

    feat: remove some fields message

--- lib/src/pages/register_page.dart
+++ lib/src/pages/register_page.dart
@@ -35,10 +35,6 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
                 Container(
                   margin: EdgeInsets.only(bottom: 20),
-                  child: Text(
-                    'Al ingresar acepta condiciones de uso.',
-                    style: TextStyle(fontWeight: FontWeight.w200),
-                  ),
                 )
               ],
             ),
